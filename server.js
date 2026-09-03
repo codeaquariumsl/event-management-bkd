60534f9d9b623839142693f4235372caafb883ac
sadeepaZincat <sadeepa.zincat@gmail.com>
2026-09-03 18:55:46 +0530

    env path changes

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../.env') });
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 import express from 'express';
 import cors from 'cors';
 
